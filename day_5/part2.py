44a4237eda71c228f51d88b0fd04d051126a85c3
Jen Gori <jgori@hotmail.co.uk>
2023-12-05 20:05:57 +0000

    Add comments to day 2 solutions: Remains somewhat unintelligible.

--- day_5/part2.py
+++ day_5/part2.py
@@ -1,3 +1,4 @@
+# this is exactly the same parsing of input that was done for the part 1 solution
 with open("input.txt") as f:
     data = f.readlines()
     breakpoints = [i for i, x in enumerate(data) if x == "\n"]
@@ -8,23 +9,24 @@ with open("input.txt") as f:
                                                     for x in data[breakpoints[i] + 2: breakpoints[i + 1]]]
             for i in range(len(breakpoints) - 1)}
 
+# create a list of seed groups -
+# each item in the list is a list containing 2 values - the start and end number (inclusive) of the seeds in a
+
 seed_groups = []
 for i, seed in enumerate(seeds):
     if i % 2 == 0:
         seed_groups.append([seeds[i], seeds[i]+seeds[i+1]-1])
-seed_groups.sort()
 
 
-def get_mapping_groups(map_num):
+def get_mapping_groups(map_num) -> list:
+    """Takes a map number (index in map_titles) and returns a list of mapping groups
+    Each mapping group is a list containing the start and end number of an (inclusive)
+    range of item values that will all follow the same mapping rule"""
     mapping_groups = []
     for i, item in enumerate(maps[map_titles[map_num]]):
-        mapping_groups.append([maps[map_titles[map_num]][i][1], maps[map_titles[map_num]][i][1] + maps[map_titles[map_num]][i][2] - 1])
+        mapping_groups.append([maps[map_titles[map_num]][i][1],
+                               maps[map_titles[map_num]][i][1] + maps[map_titles[map_num]][i][2] - 1])
     mapping_groups.sort()
-    return mapping_groups
-
-
-def divide_item_groups(map_num, item_groups):
-    mapping_groups = get_mapping_groups(map_num)
     for i in range(len(mapping_groups) - 1):
         if mapping_groups[i][1] + 1 < mapping_groups[i + 1][0]:
             mapping_groups.insert(i + 1, [mapping_groups[i][1] + 1, mapping_groups[i + 1][0] - 1])
@@ -32,6 +34,14 @@ def divide_item_groups(map_num, item_groups):
     if mapping_groups[0][0] != 0:
         mapping_groups.insert(0, [0, mapping_groups[0][0] - 1])
     mapping_groups.append([mapping_groups[-1][1], 9223372036854775807])
+    return mapping_groups
+
+
+def divide_item_groups(map_num, item_groups):
+    """Takes a map number (index in map_titles) and an item_groups list
+    (same format as seed_groups (line 15) and subdivides the item groups that it contains
+    into subgroups that all follow the same mapping rule"""
+    mapping_groups = get_mapping_groups(map_num)
     groups_divided = []
     for item_group in item_groups:
         while item_group[1] not in [group[1] for group in groups_divided]:
@@ -48,6 +58,8 @@ def divide_item_groups(map_num, item_groups):
 
 
 def mapping(map_num, item_groups):
+    """Takes item_group (this will be seeds_groups), applies all mappings, and
+    returns mapped groups (this will be ranges of locations)"""
     if map_num == len(map_titles):
         return item_groups
 
